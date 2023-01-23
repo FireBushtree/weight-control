ed8da35119480528a0dd21392d0dd20a04206423
owen.huang <firebushtree@163.com>
2023-01-23 22:55:36 +0800

    chore: add BMR formula

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { calcBMR } from './formula'
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log(
+    calcBMR({ gender: 'man', age: 27, height: 182, weight: 77.5 }),
+    calcBMR({ gender: 'woman', age: 27, height: 163, weight: 62 }),
+  )
 
   return (
     <div className="App">
